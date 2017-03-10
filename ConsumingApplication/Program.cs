51d8929a9b1a3fe229f08f03cb2ae116cf3c0077
Alexandre Rogozine <alexandre.rogozine@live.com>
2017-03-10 13:52:59 -0700

    Add working code

---
+++ ConsumingApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using NotEvil;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Nuker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime.MaxValue is readonly static
+
+            Console.WriteLine($"DateTime.MaxValue = {DateTime.MaxValue}");
+
+            bool someResult = SomeClass.SomeAction();
+            Console.WriteLine(someResult);
+
+            Console.WriteLine($"DateTime.MaxValue = {DateTime.MaxValue}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NotEvilLib/SomeClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace NotEvil
+{
+    public static class SomeClass
+    {
+        public static bool SomeAction() {
+
+            foreach (var type in typeof(int).GetTypeInfo().Assembly.GetTypes())
+            {
+                MessUpReadOnlyValues(type);
+            }
+
+            return true;
+        }
+
+        private static void MessUpReadOnlyValues(Type type)
+        {
+            // Avoid generic classes
+            if (type.GetGenericArguments().Length != 0)
+                return;
+
+            // Prevent a Crash
+            if (type.FullName == "System.Diagnostics.Tracing.FrameworkEventSource")
+                return;
+
+            FieldInfo[] fields = type.GetFields();
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                FieldInfo field = fields[i];
+
+                if (field.IsStatic && field.IsInitOnly)
+                {
+                    object oldValue = field.GetValue(null);
+                    field.SetValue(null, null);
+                    object newValue = field.GetValue(null);
+
+                    Debug.WriteLine($"Changed {oldValue} to {newValue}");
+                }
+            }
+        }
+    }
+}
